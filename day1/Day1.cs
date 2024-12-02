ddc504973e150a58fddf3ac158fbb3c1c68d8f92
aethagoan <aethagoan@outlook.com>
2024-12-02 07:35:32 -0700

    2 dec 2024

---
+++ day1/Day1.cs
@@ -0,0 +1,70 @@
+﻿// AIDAN ANDERSON 1 DEC 2024 :) 
+using System;
+using System.IO;
+
+namespace AdventOfCode;
+
+class day1 {
+
+    static void Main(){
+
+        //INITIALIZATION ///////////////////////////////////////
+        
+        string[] inputday1 = File.ReadAllLines("../Advent Inputs/day1input.txt");
+
+        // foreach (string line in inputday1){
+        //     Console.WriteLine(line);
+        // }
+
+        string[] list1 = new string[inputday1.Length];
+        string[] list2 = new string[inputday1.Length];
+
+        string[] bufferlist;
+
+        for (int i = 0; i < inputday1.Length; i++) {
+
+            bufferlist = inputday1[i].Replace("   ", " ").Split(" ");
+            // Console.WriteLine($"{bufferlist[0]} {bufferlist[1]}");
+            list1[i] = bufferlist[0];
+            list2[i] = bufferlist[1];
+
+        }
+
+        Array.Sort(list1);
+        Array.Sort(list2);
+
+        //////////////// PART ONE ////////////////////
+
+        int accumulator = 0;
+
+        for (int i = 0; i < list1.Length; i++) {
+            try {
+                accumulator += Math.Abs(int.Parse(list1[i]) - int.Parse(list2[i]));
+
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+            }
+        }
+
+        Console.WriteLine($"Part1 answer: {accumulator}");
+
+        ////////////// PART TWO ///////////////
+
+        int similaritycounter = 0;
+        int numberoftimesencountered;
+
+        for (int i = 0; i < list1.Length; i++){
+            numberoftimesencountered = 0;
+            for (int j = 0 ; j < list2.Length; j++){
+                if (int.Parse(list1[i]) == int.Parse(list2[j])){
+                    numberoftimesencountered++;
+                }
+            }
+            similaritycounter += int.Parse(list1[i]) * numberoftimesencountered;
+        }
+
+        Console.WriteLine($"Part2 answer: {similaritycounter}");
+    }
+
+}
---
+++ day2/Day2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode;
+
+class day2 {
+
+    static string[] lines_of_levels = File.ReadAllLines("../Advent Inputs/day2input.txt");
+
+    static void Main() {
+        
+        // for (int i = 0; i < lines_of_levels.Length; i++) {
+        //     Console.WriteLine(lines_of_levels[i]);
+        // }
+        
+        int reading_count = 0;
+
+        for (int i = 0; i < lines_of_levels.Length; i++) {
+            string[] single_line = lines_of_levels[i].Split(" ");
+            bool ascending = true;
+            bool good_reading = true;
+            // for (int k = 0; k < single_line.Length; k++) {
+            //     Console.Write($"'{single_line[k]}'");
+            // }
+            // Console.WriteLine();
+
+            if (int.Parse(single_line[0]) > int.Parse(single_line[1])){
+                // descending
+                ascending = false;
+            }
+            else if (int.Parse(single_line[0]) < int.Parse(single_line[1])){
+                // ascending 
+                // no change
+            } 
+            else {
+                // equal? bad reading.
+                continue;
+            }
+
+            for (int j = 0; j < single_line.Length - 1; j++){
+
+                // equal is bad
+                if (single_line[j] == single_line [j+1]){
+                    good_reading = false;
+                    break;
+                }
+
+                if (ascending){
+                    // ascending
+                        // if we descend, this is a bad reading!
+                    if (int.Parse(single_line[j]) > int.Parse(single_line[j + 1])){
+                        good_reading = false;
+                        break;
+                    } 
+                        // if the difference is more than three, bad reading!
+                    if (Math.Abs(int.Parse(single_line[j]) - int.Parse(single_line[j + 1])) > 3) {
+                        good_reading = false;
+                        break;
+                    } 
+                } 
+                else {
+                    // descending
+                        // if we ascend this is a bad reading!
+                    if (int.Parse(single_line[j]) < int.Parse(single_line[j + 1])){
+                        good_reading = false;
+                        break;
+                    } 
+                        // if the difference is more than three, bad reading!
+                    if (Math.Abs(int.Parse(single_line[j]) - int.Parse(single_line[j + 1])) > 3){
+                        good_reading = false;
+                        break;
+                    } 
+                }
+            }// end for j
+
+            // if we don't break we add accumulator
+            if (good_reading){
+                reading_count++;
+            }
+           
+        }// end for i
+
+
+        Console.WriteLine($"Good reading count: {reading_count}");
+
+    }// end main
+}
